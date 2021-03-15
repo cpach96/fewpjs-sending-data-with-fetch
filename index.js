d6020a1325e277e4cb112e16e1c917f3438e53b6
cpach96 <cpach96@gmail.com>
2021-03-15 12:52:57 -0400

    commit tree c788227021561da52238a68d281bd9238b52b5af

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+let body = document.querySelector("body")
+let h1 = document.createElement("h1")
+function submitData(name,email){
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+      
+      
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          document.body.innerHTML = object['id']
+          console.log(object["id"]);
+        })
+        .catch(function(error) {
+            alert("Error heres where things went wrong");
+           document.body.innerHTML = error.message
+            console.log(h1);
+         }); 
+    
+}
+
